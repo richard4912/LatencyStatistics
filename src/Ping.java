04353211ff34c24a25712956f273a1c850607b4c
Richard Liu <me@richard4912.tk>
2015-05-19 23:08:49 -0700

    Made ping work!

--- src/Ping.java
+++ src/Ping.java
@@ -1,55 +1,47 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.Scanner;
 
 
 public class Ping
 {
-
-    public int pingHost( String host ) throws IOException
+    public static int getLatency( String address )
     {
-        Process p1 = Runtime.getRuntime().exec( "ping -n 1 " + host );
-        int pingValue = 0;
+        String time = "";
 
-        long t1 = System.currentTimeMillis();
+        // The command to execute
+        String pingCmd = "ping " + address;
+
+        // get the runtime to execute the command
+        Runtime runtime = Runtime.getRuntime();
         try
         {
-            p1.waitFor();
-        }
-        catch ( InterruptedException e )
-        {
-            e.printStackTrace();
-        }
-        long t2 = System.currentTimeMillis();
+            Process process = runtime.exec( pingCmd );
 
-        int exit = p1.exitValue();
-        if ( exit == 0 )
-        {
-            pingValue = (int) (t2-t1-getSysLatency());
-            return pingValue;
+            // Gets the inputstream to read the output of the command
+            BufferedReader in = new BufferedReader( new InputStreamReader( process.getInputStream() ) );
+
+            // reads the outputs
+            String inputLine = in.readLine();
+            while ( ( inputLine != null ) )
+            {
+                if ( inputLine.length() > 0 && inputLine.contains( "time" ) )
+                {
+                    time = inputLine.substring( inputLine.indexOf( "time" ) );
+                    break;
+                }
+                inputLine = in.readLine();
+            }
+            time = time.substring( 5, time.length() - 3 );
+//            System.out.println( time );
         }
-        else
+        catch ( Exception ex )
         {
-            return -1;
+            System.out.println( ex );
         }
+        return Integer.parseInt( time );
     }
 
-
-    public static int getSysLatency() throws IOException
-    {
-        long t3 = System.currentTimeMillis();
-
-        Process p2 = Runtime.getRuntime().exec( "ping -n 1 127.0.0.1" );
-
-        try
-        {
-            p2.waitFor();
-        }
-        catch ( InterruptedException e )
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        t3 = System.currentTimeMillis() - t3;
-
-        return (int)t3;
-    }
 }
